4fd9605faa893c0d56336de9101b1c71a5647f6a
DaichiHoshina <hoshina0713@yahoo.co.jp>
2026-09-25 09:35:53 +0900

    GIFを全体がゆっくり呼吸する表現へ変更

--- scripts/render-glass-gif.sh
+++ scripts/render-glass-gif.sh
@@ -18,10 +18,7 @@ else
   output_height=$source_height
 fi
 
-shine_width=$((output_height / 2))
-
 ffmpeg -hide_banner -loglevel error -y \
   -loop 1 -i "$input_path" \
-  -f lavfi -i "color=c=white@0.0:s=${shine_width}x${output_height}:r=8:d=4,format=rgba,drawbox=x=0:y=0:w=${shine_width}:h=${output_height}:color=white@0.12:t=fill:replace=1" \
-  -filter_complex "[0:v]scale=${output_width}:${output_height}:flags=lanczos[base];[1:v]rotate=0.14:c=none[shine];[base][shine]overlay=x='-w+(W+w)*t/4':y=0:shortest=1,trim=duration=4,fps=8,split[a][b];[a]palettegen=stats_mode=full:max_colors=64:reserve_transparent=1:transparency_color=FFFFFF[p];[b][p]paletteuse=dither=bayer:bayer_scale=5:alpha_threshold=128" \
+  -filter_complex "[0:v]scale=${output_width}:${output_height}:flags=lanczos,eq=brightness='0.006*sin(2*PI*t/4)':contrast='1+0.010*sin(2*PI*t/4)':saturation='1+0.014*sin(2*PI*t/4)':eval=frame,trim=duration=4,fps=8,split[a][b];[a]palettegen=stats_mode=full:max_colors=64:reserve_transparent=1:transparency_color=FFFFFF[p];[b][p]paletteuse=dither=bayer:bayer_scale=5:alpha_threshold=128" \
   -loop 0 "$output_path"
